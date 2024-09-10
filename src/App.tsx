0d31532d13566a7bc8131d6ac305f894b4ef348a
Gabriel Merola <gabrielmerola12@gmail.com>
2024-09-10 12:46:23 -0300

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function App() {
   const [login, setLogin] = useState(false)
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [user, setUser] = useState("")
+
+  const data = [
+    {disciplina: "Desenv. Multiplataforma", nota: 10, faltas: 2},
+    {disciplina: "Álgebra Linear e Geometria Analítica", nota: 10, faltas: 2},
+    {disciplina: "Banco de Dados Não Relacional", nota: 10, faltas: 2},
+    {disciplina: "Engenharia de Software", nota: 10, faltas: 2},
+    {disciplina: "Teoria da Comp. Autômato e Linguagem Formal", nota: 10, faltas: 2},
+    {disciplina: "Projeto Integrador - Multiplataforma", nota: 10, faltas: 2}
+  ]
 
   function handleLogin() {
     if (email === "" || password === "") {
@@ -14,8 +24,40 @@ export default function App() {
       alert("Email inválido!")
       return
     }
-    setLogin(true)
+    const response = fetch("http://localhost:8080/api/mptas-login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email, password})
+    })
+    response.then((res) => {
+      if (res.status === 401) {
+        alert("Email ou senha inválidos!")
+        return
+      }
+      if (res.status === 200) {
+        alert("Login efetuado com sucesso!")
+        setUser(res.idUser)
+        setLogin(true)
+      }
+    })
   }
+
+  async function getData() {
+    const response = await fetch("http://localhost:8080/api/mptas-faltas?"+user, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <main className="flex items-center justify-center bg-gray-200 h-screen w-full">
@@ -42,36 +84,13 @@ export default function App() {
             <th className="border-2">Notas</th>
             <th className="border-2">Faltas</th>
           </tr>
-          <tr className="border-2">
-            <td className="border-2">Desenv. Multiplataforma</td>
-            <td className="border-2 text-center">10</td>
-            <td className="border-2 text-center">2</td>
-          </tr>
-          <tr className="border-2">
-            <td className="border-2">Álgebra Linear e Geometria Analítica</td>
-            <td className="border-2 text-center">10</td>
-            <td className="border-2 text-center">2</td>
-          </tr>
-          <tr className="border-2">
-            <td className="border-2">Banco de Dados Não Relacional</td>
-            <td className="border-2 text-center">10</td>
-            <td className="border-2 text-center">2</td>
-          </tr>
-          <tr className="border-2">
-            <td className="border-2">Engenharia de Software</td>
-            <td className="border-2 text-center">10</td>
-            <td className="border-2 text-center">2</td>
-          </tr>
-          <tr className="border-2">
-            <td className="border-2">Teoria da Comp. Autômato e Linguagem Formal</td>
-            <td className="border-2 text-center">10</td>
-            <td className="border-2 text-center">2</td>
-          </tr>
-          <tr className="border-2">
-            <td className="border-2">Projeto Integrador - Multiplataforma</td>
-            <td className="border-2 text-center">10</td>
-            <td className="border-2 text-center">2</td>
-          </tr>
+          {data.map((item, index) => (
+            <tr key={index} className="border-2">
+              <td className="border-2">{item.disciplina}</td>
+              <td className="border-2 text-center">{item.nota}</td>
+              <td className="border-2 text-center">{item.faltas}</td>
+            </tr>
+          ))}
         </table>
         <button onClick={() => setLogin(false)} className="bg-red-500 rounded-full p-2 text-white px-8 hover:bg-red-600">Sair</button>
       </div>
